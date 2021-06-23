8b83f9b270fdaa7760485a26cb4931669c34159c
FernandoChahuaEveris <fchahual@everis.com>
2021-06-23 10:16:27 -0500

    checkstyle and eurekaclient
    Add checkstyle and eurekaclient in transaction service

--- transactionservice/src/main/java/com/everis/transactionservice/TransactionserviceApplication.java
+++ transactionservice/src/main/java/com/everis/transactionservice/TransactionserviceApplication.java
@@ -2,8 +2,10 @@ package com.everis.transactionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TransactionserviceApplication {
 
 	public static void main(String[] args) {
